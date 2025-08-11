3c9f16cfa6295279ab2e5d3b09abbb81ffc6bb3f
SaanDev <sahanslst@gmail.com>
2025-08-11 19:13:55 +0530

    Now data can be saved as an excel file.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
+import os
 from PySide6.QtWidgets import QApplication
 from gui_main import MainWindow
-import sys
 import faulthandler
+
+# Fix for PyInstaller GUI mode when console=False
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, 'w')
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, 'w')
 
 faulthandler.enable()
 
