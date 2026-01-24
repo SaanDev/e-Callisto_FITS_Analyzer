b431b3c6eeb88e1fa6fe30b39082577a90182035
Sahan S Liyanage <sahanslst@gmail.com>
2026-01-24 13:06:03 +0530

    chore: Update project version from 1.7.6 to 1.7.7 and mark as development version across all files.

--- src/Installation/setup.py
+++ src/Installation/setup.py
@@ -1,9 +1,10 @@
 """
 e-CALLISTO FITS Analyzer
-Version 1.7.6
+Version 1.7.7 - Development Version
 Sahan S Liyanage (sahanslst@gmail.com)
 Astronomical and Space Science Unit, University of Colombo, Sri Lanka.
 """
+
 import os
 import sys
 from setuptools import setup
